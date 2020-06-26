2d2e99bed376e8dca932c07fdaba027b66c8c0cf
dev-jackson <angeljackson0933@gmail.com>
2020-06-26 17:48:28 -0500

    Add useraction parcial

--- config/settings.php
+++ config/settings.php
@@ -43,7 +43,6 @@ $settings['db'] =[
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci'
     ]
-
 ];
 
 return $settings;
